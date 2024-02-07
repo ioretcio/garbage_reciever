a5a239396a168fc913da0078e3115886dd2d477b
ioretcio <goncharukyou@gmail.com>
2024-02-07 12:43:20 +0000

    200 images

--- garbagestorer.py
+++ garbagestorer.py
@@ -6,6 +6,8 @@ import logging
 app = Flask(__name__)
 app.config["DEBUG"] = False
 url = "http://194.35.13.94:5000/getmail"
+importance0 = "http://194.35.13.94:5000/lowImportance"
+checkImportance = "http://194.35.13.94:5000/checkImportance"
 rootdir = "/srv/ftp/mediastorage"
 log_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'service.log')
 print(log_file_path)
@@ -27,8 +29,6 @@ printLog("Starting service")
 
 @app.route('/sendMedia', methods=['POST'])
 def upload_file():
-    print("TYR")
-    
     if 'file' not in request.files:
         return Response(json.dumps({"status": "No file"}), status=400)
     file = request.files['file']
@@ -44,6 +44,17 @@ def upload_file():
         email = response_json.get('email')
         if not os.path.exists(os.path.join(rootdir, email)):
             os.mkdir(os.path.join(rootdir, email))
+
+        if len(os.listdir(os.path.join(rootdir, email)))>200:
+            response = requests.get(checkImportance, headers=headers)
+            if response.status_code == 201:
+                requests.post(importance0, headers=headers)
+                return Response("User are not important", status=401) 
+            elif response.status_code == 202: pass
+            elif response.status_code == 401: 
+                return Response("User are not important", status=401) 
+            
+            
         if not os.path.exists(os.path.join(rootdir, email,file.filename )):
             file.save(os.path.join(rootdir, email, file.filename))
             return Response("ok", status=200) 
